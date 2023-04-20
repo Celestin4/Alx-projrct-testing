4f1c77792e26c729555c6269659f425f8a481fb9
Celestin4 <nteziryayocelestin3@gmail.com>
2023-04-20 17:35:42 +0200

    creating one source of headers

--- shell.h
+++ shell.h
@@ -1,32 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+// standard library headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
-#define MAX_COMMAND_LENGTH 1024
+// maximum number of arguments for a command
+#define MAX_ARGS 64
 
-int main() {
-    char command[MAX_COMMAND_LENGTH];
-    char *args[2];
-    args[1] = NULL;
-    
-    while (1) {
-        printf("#cisfun$ ");
-        
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-            // end of file reached
-            exit(0);
-        }
-        
-        // remove newline character from input
-        command[strcspn(command, "\n")] = '\0';
-        
-        args[0] = strtok(command, " ");
-        
-        if (execvp(args[0], args) == -1) {
-            printf("%s: No such file or directory\n", args[0]);
-        }
-    }
-    
-    return 0;
-}
+// declaration of the 'environ' variable
+extern char **environ;
+
+
+
+#endif /* SHELL_H */
+
